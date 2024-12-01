0906a95e96aea353ef776f1b0f1fed7c9a74e827
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2024-12-02 00:05:37 +0100

    Ajout interface dev, ajustement lighting
    Ajou d'une interface dev sur la main gauche, avec FPS et nombre de triangles dans la scène. Amélioration rendu pour avoir une meilleure réflexion de lumières.

---
+++ Assets/Scripts/Renderers/DevUIRenderer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DevUIRenderer : MonoBehaviour
+{
+    int avgFPS;
+    int triangleCount;
+    [SerializeField]
+    private TextMeshProUGUI fpsDisplay;
+    [SerializeField]
+    private TextMeshProUGUI triangleDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // On répète l'exécution de ces méthodes chaque seconde.
+        InvokeRepeating("DisplayFrameRate", 1, 1);
+        InvokeRepeating("DisplayTriangleCount", 1, 1);
+    }
+
+    /// <summary>
+    /// Cette méthode change le texte du champs de FPS pour afficher le nombre de Frames Par Seconde.
+    /// </summary>
+    private void DisplayFrameRate()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFPS = (int)current;
+        fpsDisplay.text = "FPS : " + avgFPS;
+    }
+
+    /// <summary>
+    /// Cette méthode change le texte du champs de triangles pour afficher le nombre de triangles dans la scène.
+    /// </summary>
+    private void DisplayTriangleCount()
+    {
+        triangleCount = 0;
+        MeshFilter[] allMeshes = FindObjectsByType<MeshFilter>(FindObjectsSortMode.None);
+
+        foreach (MeshFilter mesh in allMeshes)
+        {
+            triangleCount += mesh.mesh.triangles.Length;
+        }
+        triangleDisplay.text = "Triangles : " + triangleCount;
+    }
+}
